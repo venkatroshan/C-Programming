77a12956160d5fc2bf1d1a510ad1aee51a937821
priyanga24 <34644984+priyanga24@users.noreply.github.com>
2017-12-19 15:48:00 +0530

    Update prime.c

--- code katta/beginner/prime.c
+++ code katta/beginner/prime.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 int main() 
 {
-int n,i=1;
+int n,i;
 scanf("%d",&n);
+int flag=1;
 for(i=2;i<n/2;i++)
 {
 
     if(n%i==0)
     {
-        printf("not prime number");
+       flag=0;
     }
     else
     {
-        printf("prime number");
+       flag=1;
     }
+}
+if(flag==1)
+{
+    printf("prime");
+}
+else
+{
+    printf("not prime");
 }
 }
