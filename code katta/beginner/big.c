de0be91c88ccf8be84d647c382dfaf3bcec3194f
priyanga24 <34644984+priyanga24@users.noreply.github.com>
2017-12-19 14:33:22 +0530

    Create big.c

---
+++ code katta/beginner/big.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	
+	int a,b,c;
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+int max;
+if(a>b && a>c)
+{
+printf("%d is greater",a);
+}
+else if(b>a && b>c)
+{
+printf("%d is greater",b);
+}
+else
+{
+printf("%d is greater",c);
+}
+	return 0;
+
+}
