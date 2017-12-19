43842ec32ce984f3cfb92e10d9473e909207698f
priyanga24 <34644984+priyanga24@users.noreply.github.com>
2017-12-19 14:27:46 +0530

    Create alpha.c

---
+++ code katta/beginner/alpha.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	
+	char c;
+	scanf("%c",&c);
+	if(c>='a' && c<='z' || c>='A' && c<='Z')
+	{
+	printf("alphabet");
+	}
+else
+{
+printf("not alphabet");
+}
+
+	return 0;
+
+}
