f54cdccb2fed33713ce6300780170e001b152eb6
priyanga24 <34644984+priyanga24@users.noreply.github.com>
2017-12-21 00:46:55 +0530

    Create rev.c

---
+++ code katta/beginner/rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include<math.h>
+int main() 
+{
+int n;
+scanf("%d",&n);
+int temp,mod,sum=0;
+temp=n;
+while(temp>0)
+{
+mod=temp%10;
+sum=sum*10+mod;
+temp=temp/10;
+}
+printf("%d",sum);
+return 0;
+}
