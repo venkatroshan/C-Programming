b3b9235d9750ff68a7e10db759f27957753b8b40
priyanga24 <34644984+priyanga24@users.noreply.github.com>
2018-01-20 10:17:56 +0530

    Create strr.c

---
+++ code katta/beginner/strr.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[50];
+    int n,i;
+    scanf("%s",str);
+    scanf("%d",&n);
+    int l=strlen(str);
+    for(i=0;i<n;i++)
+    {
+   printf("%c",str[i]);
+    }
+         
+}
