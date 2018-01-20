a6f5badc8acbccc1440b2c8ee7b2de2c336e2c74
priyanga24 <34644984+priyanga24@users.noreply.github.com>
2018-01-20 10:13:54 +0530

    Create fii.c

---
+++ code katta/beginner/fii.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,f1=0,f2=1,fibo;
+    scanf("%d",&n);
+    printf("%d\n%d\n",f1,f2);
+    int i,j;
+    for(i=2;i<n;i++)
+    {
+        fibo=f1+f2;
+        printf("%d\n",fibo);
+        f1=f2;
+        f2=fibo;
+        }
+        return 0;
+}
