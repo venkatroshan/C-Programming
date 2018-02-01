73893048c591eb0408c61ea84d25df7890672682
priyanga24 <34644984+priyanga24@users.noreply.github.com>
2018-02-01 11:36:14 +0530

    Create 106.c

---
+++ code katta/beginner/106.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(void)
+{
+int n,i,count=0;
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+if(n%i==0)
+{
+count=1;
+break;
+}
+}
+if(count==0)
+{
+printf("no");
+}
+else
+{
+printf("yes");
+}
+return 0;
+}
