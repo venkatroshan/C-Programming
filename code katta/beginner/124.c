1eff428b7df96e8781d8c12646592770b358927f
priyanga24 <34644984+priyanga24@users.noreply.github.com>
2018-02-01 12:04:47 +0530

    Create 124.c

---
+++ code katta/beginner/124.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+   int a[10],n,k,count=0,i;
+   scanf("%d %d",&n,&k);
+   for(i=0;i<n;i++)
+   {
+   scanf("%d",&a[i]);
+   }
+   for(i=0;i<n;i++)
+   {
+       if(a[i]%2!=0)
+       count++;
+   
+   if(count==k)
+   {
+   printf("%d",a[i]);
+   break;
+   }
+   }
+   
+}
