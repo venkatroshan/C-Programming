54f3b4346318a14db019fa56e1b2cb01ff919d38
priyanga24 <34644984+priyanga24@users.noreply.github.com>
2018-02-02 09:44:58 +0530

    Create strrev.java

---
+++ code katta/beginner/strrev.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    Scanner sc=new Scanner(System.in);
+
+	    StringBuffer sb=new StringBuffer(sc.nextLine());
+	    int i;
+	    String str="";
+	 sb.reverse();
+	    System.out.print(sb);
+	    
+
+	}
+}
