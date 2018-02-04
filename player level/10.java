6e136c3947201f12ba4a82fb21c533b14f2ca146
priyanga24 <34644984+priyanga24@users.noreply.github.com>
2018-02-04 09:35:52 +0530

    Create 10.java

---
+++ player level/10.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		String s1=sc.nextLine();
+    
+    int i;
+		for(i=0;i<s.length() && i<s1.length();i++)
+		{
+		    
+		if(s.charAt(i)!=s1.charAt(i))
+		{
+		    System.out.println("ys");
+		}
+		else{
+		    break;
+		}
+		
+		
+}
+	}
+}
