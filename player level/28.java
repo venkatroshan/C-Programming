37ad736575bea0afe7db39ac98369618cb380a18
priyanga24 <34644984+priyanga24@users.noreply.github.com>
2018-02-10 18:36:07 +0530

    Create 28.java

---
+++ player level/28.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//System.out.println("GfG!");
+		Scanner sc=new Scanner(System.in);
+		int n,j;
+		String s=sc.nextLine();
+		String sr[]=s.split("\\s");
+		int i;
+		for(i=0;i<s.length();i++)
+		System.out.print(sr[i]);
+		
+		
+		
+		
+		
+	
+	}
+}
